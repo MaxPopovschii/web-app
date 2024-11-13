5bba91ebfee2da9f32eb680e42e1a3768713cdef
Maxim Popovschii <maxim.popovschii@comelit.it>
2024-11-13 19:28:27 +0100

    added DTO

---
+++ backend/src/main/java/com/web/web_app/dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.web.web_app.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+}
---
+++ backend/src/main/java/com/web/web_app/utilities/EcoFactors.java
@@ -0,0 +1,5 @@
+package com.web.web_app.utilities;
+
+public enum EcoFactors {
+    
+}
